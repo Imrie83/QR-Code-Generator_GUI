398e1c9b25f23b6f92e59fe386d586c284281440
Marcin Zielinski <51891361+Imrie83@users.noreply.github.com>
2021-09-17 09:45:31 +0200

    Update QRGen_GUI.py
    PEP8 cleaned code

--- QRGen_GUI.py
+++ QRGen_GUI.py
@@ -14,11 +14,11 @@ link = ""
 
 def submit():
     link = link_var.get()
-    qr = qrcode.QRCode(version = 3, error_correction=qrcode. \
-        constants.ERROR_CORRECT_L, box_size=25, border = 2,)
+    qr = qrcode.QRCode(version=3, error_correction=qrcode.
+                       constants.ERROR_CORRECT_L, box_size=25, border=2,)
     qr.add_data(link)
     qr.make(fit=True)
-    img = qr.make_image(fill_color = "black", back_color = "white")
+    img = qr.make_image(fill_color="black", back_color="white")
     for c in ['\"', '/', ':', '.']:
         if c in link:
             link = link.replace(c, '')
